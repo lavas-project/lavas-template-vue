309b2f2315924e22e594598a7e88d14289f8dc46
xiaoiver <pyqiverson@gmail.com>
2017-11-28 14:16:11 +0800

    restore middleware

--- pages/appshell/main.vue
+++ pages/appshell/main.vue
@@ -5,8 +5,7 @@
 export default {
     name: 'appshell-main',
     head: {
-        title: 'Home',
-        titleTemplate: '%s - Lavas',
+        title: 'Lavas',
         meta: [
             {name: 'keywords', content: 'lavas PWA'},
             {name: 'description', content: '基于 Vue 的 PWA 解决方案，帮助开发者快速搭建 PWA 应用，解决接入 PWA 的各种问题'}
