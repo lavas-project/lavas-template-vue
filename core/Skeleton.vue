f54075cd4016c87e7f65320c5bcd838b4729e879
chenqiushi <tracy909@126.com>
2018-01-02 11:08:28 +0800

    fix: change skeleton header color

--- core/Skeleton.vue
+++ core/Skeleton.vue
@@ -27,7 +27,7 @@ export default {
 
     .skeleton-header
         height 52px
-        background blue
+        background #1976d2
         box-shadow 0 2px 4px -1px rgba(0,0,0,.2), 0 4px 5px rgba(0,0,0,.14), 0 1px 10px rgba(0,0,0,.12)
 
     .skeleton-block
