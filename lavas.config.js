d428457e85c2d1754f21f3fd6d545543a9f19ac1
Wangyisheng <wangyisheng@baidu.com>
2018-01-04 15:47:18 +0800

    自动生成 service-worker.js 头尾

--- lavas.config.js
+++ lavas.config.js
@@ -64,7 +64,7 @@ module.exports = {
             'sw-register.js',
             '**/*.map'
         ],
-        appshellUrls: ['/appshell'],
+        appshellUrl: '/appshell',
         dontCacheBustUrlsMatching: /\.\w{8}\./
     }
 };
