36c133158c0066b10fb278b6642014f8e523641e
Wangyisheng <wangyisheng@baidu.com>
2018-01-05 15:42:47 +0800

    core/service-worker.js 增加一些说明

--- core/service-worker.js
+++ core/service-worker.js
@@ -13,3 +13,28 @@ const workboxSW = new WorkboxSW({
 
 // Define precache injection point.
 workboxSW.precache([]);
+
+/**
+ * example runningCache with api
+ */
+// workboxSW.router.registerRoute(/^https:\/\/lavas\.baidu\.com\/some\/api/,
+//     workboxSW.strategies.networkFirst());
+
+
+/**
+ * example runningCache with resources from CDN
+ * including maxAge, maxEntries
+ * cacheableResponse is important for CDN
+ */
+// workboxSW.router.registerRoute(/^https:\/\/cdn\.baidu\.com/i,
+//     workboxSW.strategies.cacheFirst({
+//         cacheName: 'lavas-cache-images',
+//         cacheExpiration: {
+//             maxEntries: 100,
+//             maxAgeSeconds: 7 * 24 * 60 * 60
+//         },
+//         cacheableResponse: {
+//             statuses: [0, 200]
+//         }
+//     })
+// );
