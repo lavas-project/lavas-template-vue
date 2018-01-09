961f692476ad080db08670c4e6e39c6be79d3f2e
Wangyisheng <wangyisheng@baidu.com>
2018-01-09 16:51:16 +0800

    feat: add config to lavas dev/start/build

--- server.prod.js
+++ server.prod.js
@@ -11,20 +11,23 @@ let port = process.env.PORT || 3000;
 
 let core = new LavasCore(__dirname);
 
-core.init(process.env.NODE_ENV || 'production')
-    .then(() => core.runAfterBuild())
-    .then(() => {
-        app.use(core.expressMiddleware());
-        app.listen(port, () => {
-            console.log('server started at localhost:' + port);
+module.exports = options => {
+    core.init(process.env.NODE_ENV || 'production')
+        .then(() => core.runAfterBuild())
+        .then(() => {
+            app.use(core.expressMiddleware());
+            app.listen(port, () => {
+                console.log('server started at localhost:' + port);
+            });
+        }).catch(err => {
+            console.log(err);
         });
-    }).catch(err => {
-        console.log(err);
-    });
 
-// catch promise error
-process.on('unhandledRejection', (err, promise) => {
-    console.log('in unhandledRejection');
-    console.log(err);
-    // cannot redirect without ctx!
-});
+    // catch promise error
+    process.on('unhandledRejection', (err, promise) => {
+        console.log('in unhandledRejection');
+        console.log(err);
+        // cannot redirect without ctx!
+    });
+}
+
