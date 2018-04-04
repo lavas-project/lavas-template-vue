9076ee7ac99c3256e09881426a484489fc4a81ba
xiaoiver <pyqiverson@sina.com>
2018-04-04 17:04:25 +0800

    fix: scroll behavior (#47)
    * fix: scroll behavior
    
    * Update lavas.config.js

--- core/App.vue
+++ core/App.vue
@@ -14,14 +14,16 @@
             <transition
                 :name="pageTransitionEffect"
                 @before-enter="handleBeforeEnter"
-                @after-enter="handleAfterEnter">
+                @after-enter="handleAfterEnter"
+                @before-leave="handleBeforeLeave">
                 <keep-alive
                     :include="[...keepAlivePages]">
                     <router-view
                         :key="routerViewKey"
                         class="app-view"
                         :class="[{'app-view-with-header': appHeaderShow}, pageTransitionClass]"
-                        ></router-view>
+                        :data-page-id="$route.fullPath"
+                    ></router-view>
                 </keep-alive>
             </transition>
             <update-toast></update-toast>
@@ -30,11 +32,14 @@
 </template>
 
 <script>
+import Vue from 'vue';
 import {mapState, mapActions} from 'vuex';
 import AppHeader from '@/components/AppHeader';
 import AppSidebar from '@/components/AppSidebar';
 import UpdateToast from '@/components/UpdateToast';
 import {keepAlivePages} from '@/.lavas/router';
+
+const ENABLE_SCROLL_CLASS = 'app-view-scroll-enabled';
 
 export default {
     name: 'app',
@@ -51,6 +56,10 @@ export default {
 
         ...mapState('appShell/appHeader', {
             appHeaderShow: state => state.show
+        }),
+
+        ...mapState('appShell/common', {
+            scrollPostionMap: state => state.scrollPostionMap
         }),
 
         pageTransitionClass() {
@@ -79,13 +88,55 @@ export default {
             'hideSidebar'
         ]),
         ...mapActions('appShell/common', [
-            'setPageSwitching'
+            'setPageSwitching',
+            'savePageScrollPosition'
         ]),
+
+        /**
+         * make current page container scrollable,
+         * and restore its scroll position.
+         */
+        restoreContainerScrollPosition(containerEl, scrollTop) {
+            containerEl.classList.add(ENABLE_SCROLL_CLASS);
+            containerEl.scrollTop = scrollTop;
+        },
+
+        /**
+         * make body scrollable,
+         * and restore its scroll position.
+         */
+        restoreBodyScrollPosition(containerEl, scrollTop) {
+            containerEl.classList.remove(ENABLE_SCROLL_CLASS);
+            document.body.scrollTop = document.documentElement.scrollTop = scrollTop;
+        },
+
         handleBeforeEnter(el) {
+            let pageId = el.dataset.pageId;
+            let {y: scrollTop = 0} = this.scrollPostionMap[pageId] || {};
+
             this.setPageSwitching(true);
+            Vue.nextTick(() => {
+                this.restoreContainerScrollPosition(el, scrollTop);
+            });
+
         },
         handleAfterEnter(el) {
+            let pageId = el.dataset.pageId;
+            let {y: scrollTop = 0} = this.scrollPostionMap[pageId] || {};
+
             this.setPageSwitching(false);
+            this.restoreBodyScrollPosition(el, scrollTop);
+        },
+        handleBeforeLeave(el) {
+            let pageId = el.dataset.pageId;
+            let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+
+            this.restoreContainerScrollPosition(el, scrollTop);
+            // save current scroll position in a map
+            this.savePageScrollPosition({
+                pageId,
+                scrollPosition: {y: scrollTop}
+            });
         },
         handleClickHeaderBack() {
             this.$router.go(-1);
@@ -105,12 +156,26 @@ export default {
 
 <style lang="stylus">
 @import '~@/assets/stylus/variable'
+
+$page-transition-duration = 0.35s
+
 #app
     font-family 'Avenir', Helvetica, Arial, sans-serif
     -webkit-font-smoothing antialiased
     -moz-osx-font-smoothing grayscale
     text-align center
     color #2c3e50
+    height 100%
+
+    .application--wrap
+        height 100%
+        min-height 100%
+
+    .app-shell-header
+        position fixed
+        right 0
+        left 0
+        z-index 140
 
     .app-view
         position absolute
@@ -118,8 +183,8 @@ export default {
         right 0
         bottom 0
         left 0
-        overflow-x hidden
-        overflow-y auto
+        -webkit-overflow-scrolling touch
+        background white
 
         &::-webkit-scrollbar
             width 0
@@ -129,19 +194,33 @@ export default {
             top $app-header-height
 
         &.transition-slide
-            transition transform 0.4s cubic-bezier(.55, 0, .1, 1)
+            transition transform $page-transition-duration cubic-bezier(0, 0, 0.2, 1)
 
             &.slide-left-enter
                 transform translate(100%, 0)
 
+            &.slide-left-enter-active
+                box-shadow 0 0 16px 2px rgba(0, 0, 0, 0.3)
+
             &.slide-right-enter
-                transform translate(-100%, 0)
+                transform translate(-30%, 0)
+                transition-timing-function linear
 
             &.slide-right-leave-active
                 transform translate(100%, 0)
+                box-shadow 0 0 16px 2px rgba(0, 0, 0, 0.3)
+                z-index 99
 
             &.slide-left-leave-active
-                transform translate(-100%, 0)
+                transform translate(-30%, 0)
+                transition-timing-function linear
+
+            &.app-view-scroll-enabled,
+            &.slide-left-enter-active,
+            &.slide-left-leave-active,
+            &.slide-right-enter-active,
+            &.slide-right-leave-active
+                overflow-y auto
 
         &.transition-fade
             opacity 1
@@ -152,16 +231,4 @@ export default {
 
             &.fade-leave-active
                 opacity 0
-
-        // &.transition-slide-fade
-        //     &.slide-fade-enter-active
-        //         transition: all .3s ease
-        //
-        //     &.slide-fade-leave-active
-        //         transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)
-        //
-        //     &.slide-fade-enter,
-        //     &.slide-fade-leave-to
-        //         transform: translateX(10px)
-        //         opacity: 0
 </style>
